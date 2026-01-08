1529df64abad1ae7527e62fafbf7b18637184023
Ram_Sir <ramswaroopdanta@gmail.com>
2026-01-08 19:17:35 +0530

    change font size
    rscit font size change

--- src/components/blocks/hero/HeroRscit.tsx
+++ src/components/blocks/hero/HeroRscit.tsx
@@ -13,7 +13,7 @@ export default function HeroRscit() {
             data-group="page-title"
             data-interval="-200"
             data-delay="500">
-            <h2 className="display-1 fs-56 mb-4 mt-0 mt-lg-5 ls-xs pe-xl-5 pe-xxl-0">
+            <h2 className="display-4 fs-56 mb-4 mt-0 mt-lg-5 ls-xs pe-xl-5 pe-xxl-0">
               Rajasthan State Certificate in Information Technology <span className="underline-3 style-3 yellow">(RSCIT)</span>
             </h2>
             <p className="lead fs-23 lh-sm mb-7 pe-lg-5 pe-xl-5 pe-xxl-0">
